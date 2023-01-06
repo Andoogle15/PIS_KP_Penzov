78df410f2feb3d15f108aef931c8a2ca9a2b457e
Andoogle15 <penzov.r.i.20@gmail.com>
2023-01-06 19:56:30 +0300

    test9

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://foodcenter-pis.onrender.com'),
 
     'asset_url' => env('ASSET_URL'),
 
